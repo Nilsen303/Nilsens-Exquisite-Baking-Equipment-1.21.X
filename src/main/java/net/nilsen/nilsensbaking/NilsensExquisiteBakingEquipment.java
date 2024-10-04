9c27a6c4d41ae87f5973db7693dd2a418e4db6ca
nilsw <nilsbwallerberg@gmail.com>
2024-10-04 23:23:51 +0200

    preparation for item adding

--- src/main/java/net/nilsen/nilsensbaking/NilsensExquisiteBakingEquipment.java
+++ src/main/java/net/nilsen/nilsensbaking/NilsensExquisiteBakingEquipment.java
@@ -17,6 +17,6 @@ public class NilsensExquisiteBakingEquipment implements ModInitializer {
 
 
 
-		LOGGER.info("initialized" + MOD_ID);
+		LOGGER.info("Nilsen is not responsible for any medical problems you may get from looking att the absolute mess of code that i shamelessly copied for" + MOD_ID);
 	}
 }
